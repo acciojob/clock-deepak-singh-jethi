4ebbeb414ec8e864f601a4a6f13677deddf05762
DEEPAK SINGH JETHI <134581381+deepak-singh-jethi@users.noreply.github.com>
2023-12-10 16:47:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const timer = document.querySelector("#timer");
+
+setInterval(() => {
+  let instance = new Date();
+
+  const date = `${
+    instance.getMonth() + 1
+  }/${instance.getDate()}/${instance.getFullYear()}`;
+
+  let hours = instance.getHours();
+  let ampm = hours > 12 && hours != 24 ? "PM" : "AM";
+  hours = hours <= 12 ? hours : hours - 12;
+
+  const time = `${hours}:${instance.getMinutes()}:${instance.getSeconds()} ${ampm}`;
+
+  timer.innerText = `${date} ${time}`;
+}, 1000);
